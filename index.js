ab475893c0479a033dc869844a5a04cc60488a81
nechami hart <n0527142469@gmail.com>
2026-09-02 19:27:08 +0300

    Creating a MongoDB connection function and defining some environment variables.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import {env} from './config/env.js'
+import {connectDB } from './config/db.js';
 
-const app =express()
+const app =express();
 
+connectDB();
 
-app.listen(5000 ,()=>{
-    console.log(`Server is running on http://localhost:5000`)
+app.use(express.json());
+
+app.listen(env.PORT ,()=>{
+    console.log(`Server is running on http://localhost:${env.PORT}`)
 })
